e4158e22bd122fd803cc3fee1db2722886e44c0b
parasquid <tristan.gomez@gmail.com>
2017-01-29 02:53:48 +0800

    add tests for the method hmac_sha1
    this calculates the intermediate HMAC value
    
    from RFC4226:
    
       Table 1 details for each count, the intermediate HMAC value.
    
       Count    Hexadecimal HMAC-SHA-1(secret, count)
       0        cc93cf18508d94934c64b65d8ba7667fb7cde4b0
       1        75a48a19d4cbe100644e8ac1397eea747a2d33ab
       2        0bacb7fa082fef30782211938bc1c5e70416ff44
       3        66c28227d03a2d5529262ff016a1e6ef76557ece
       4        a904c900a64b35909874b33e61c5938a8e15ed1c
       5        a37e783d7b7233c083d4f62926c7a25f238d0316
       6        bc9cd28561042c83f219324d3c607256c03272ae
       7        a4fb960c0bc06e1eabb804e5b397cdc4b45596fa
       8        1b3c89f65e6c9e883012052823443f048b4332db
       9        1637409809a679dc698207310c8c7fc07290d9e5

---
+++ spec/feature_spec.rb
@@ -0,0 +1,79 @@
+require "spec_helper"
+
+describe OneTimePasswordAlgorithm do
+  Given(:klass) { OneTimePasswordAlgorithm }
+  Given(:secret) { "12345678901234567890" }
+
+  describe "hmac_sha1" do
+    When(:result) { klass.hmac_sha1(secret, count) }
+
+    context "count 0" do
+      Given(:count) { 0 }
+      Then { result == 0xcc93cf18508d94934c64b65d8ba7667fb7cde4b0 }
+    end
+
+    context "count 1" do
+      Given(:count) { 1 }
+      Then { result == 0x75a48a19d4cbe100644e8ac1397eea747a2d33ab }
+    end
+
+    context "count 2" do
+      Given(:count) { 2 }
+      Then { result == 0x0bacb7fa082fef30782211938bc1c5e70416ff44 }
+    end
+
+    context "count 3" do
+      Given(:count) { 3 }
+      Then { result == 0x66c28227d03a2d5529262ff016a1e6ef76557ece }
+    end
+
+    context "count 4" do
+      Given(:count) { 4 }
+      Then { result == 0xa904c900a64b35909874b33e61c5938a8e15ed1c }
+    end
+
+    context "count 5" do
+      Given(:count) { 5 }
+      Then { result == 0xa37e783d7b7233c083d4f62926c7a25f238d0316 }
+    end
+
+    context "count 6" do
+      Given(:count) { 6 }
+      Then { result == 0xbc9cd28561042c83f219324d3c607256c03272ae }
+    end
+
+    context "count 7" do
+      Given(:count) { 7 }
+      Then { result == 0xa4fb960c0bc06e1eabb804e5b397cdc4b45596fa }
+    end
+
+    context "count 8" do
+      Given(:count) { 8 }
+      Then { result == 0x1b3c89f65e6c9e883012052823443f048b4332db }
+    end
+
+    context "count 9" do
+      Given(:count) { 9 }
+      Then { result == 0x1637409809a679dc698207310c8c7fc07290d9e5 }
+    end
+  end
+
+  descruve "generateOTP" do
+    Given(:code_digits) { 6 }
+    Given(:add_checksum) { false }
+    GIven(:truncation_offfset) { 4 }
+    When(:result) {
+      klass.generateOTP(
+        secret,
+        moving_factor,
+        code_digits,
+        add_checksum,
+        truncation_offfset
+      )
+    }
+
+    context "count 0" do
+      Given(:moving_factor) { 0 }
+    end
+  end
+end
