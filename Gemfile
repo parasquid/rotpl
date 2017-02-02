d4300b037de99eef728d41305d10200e9b39c771
parasquid <tristan.gomez@gmail.com>
2017-02-02 16:59:08 +0800

    decode the key in the qr codes and the provisioner url
    according to http://stackoverflow.com/questions/34001567/google-authenticator-code-does-not-match-server-generated-code
    the code is base32 encoded, so we need to decode the result

--- Gemfile
+++ Gemfile
@@ -4,4 +4,5 @@ group :development, :test do
   gem "rspec"
   gem "rspec-given"
   gem "pry"
+  gem "base32"
 end
--- lib/totp.rb
+++ lib/totp.rb
@@ -1,14 +1,20 @@
 require_relative "hotp"
+require "base32"
 
 module Rotpl
   class Totp
-    def initialize(secret, hotp: Hotp)
-      @secret = secret
+    def initialize(secret, hotp: Hotp, time_step: 30)
+      @secret = Base32.decode(secret.delete(" ").upcase)
       @hotp = hotp
+      @time_step = time_step
     end
 
-    def generate_otp(time = Time.now.to_i, code_digits: 6)
-      @hotp.generate_otp(@secret, time.to_i, code_digits: code_digits)
+    def generate_otp(time = Time.now, code_digits: 6)
+      time = time.to_i
+      moving_factor = (time / @time_step).floor
+      [-1, 0, 1].map { |offset|
+        @hotp.generate_otp(@secret, moving_factor + offset, code_digits: code_digits)
+      }
     end
   end
 end
